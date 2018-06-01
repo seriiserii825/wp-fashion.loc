a485ce48c42f86283f21a156f2d8edff331a61ae
serii <seriiburduja@gmail.com>
2018-06-01 10:56:20 +0300

    вывел записи на главной

--- wp-content/themes/fashion/index.php
+++ wp-content/themes/fashion/index.php
@@ -2,11 +2,37 @@
     <div class="content-main">
         <?php if(!dynamic_sidebar( 'slider-top' )): ?>
             <h2>Добавьте элементы слайдера через виджет текст!!!</h2>    
-        <?php endif; ?>>
+        <?php endif; ?>
                 
         <div class="content-main-blocks">
+            <?php $posts_index = new WP_Query([
+                    'posts_per_page' => 3,
+                    'post_type' => ['post', 'page'],
+                    'meta_key' => 'order',
+                    'orderby' => 'meta_value_num',
+                    'order' => 'ASC'
+                ]); ?>
+
+                <?php if ( $posts_index->have_posts() ) : while ( $posts_index->have_posts() ) : $posts_index->the_post(); ?>
+                    <div>
+                        <a href="<?php the_permalink(); ?>">
+                            <!--<img src="<?php bloginfo('template_url'); ?>/images/mail-news1.jpg" alt="" />-->
+                        </a>
+                        <h1>
+                            <a href="<?php the_permalink(); ?>">
+                                <?php echo get_post_meta( get_the_ID(), 'title', true ); ?>
+                            </a>
+                        </h1>
+                        <?php the_excerpt(); ?>
+                        <p><a href="<?php the_permalink(); ?>" class="read-more">read more</a></p>
+                    </div>  
+                  <?php endwhile; ?>
+                  <!-- post navigation -->
+                  <?php else: ?>
+                  <!-- no posts found -->
+                <?php endif; ?>
         
-            <div>
+            <!--<div>
                 <a href="#"><img src="<?php bloginfo('template_url'); ?>/images/mail-news1.jpg" alt="" /></a>
                 <h1><a href="#">Hi, My Name Is Anne Photographer</a></h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisi odio, sollicitudin at condimentum at, mattis a ligula. </p>
@@ -28,7 +54,7 @@
                 <p>Donec mollis suscipit nisi, vitae sagittis arcu euismod at. In at turpis vel odio fringilla vulputate at ac tellus. Phasellus nulla leo, vehicula sit amet congue eget, viverra ut augue epharetra aliquet turpis vel scelerisque. 
 Nullam ut enim nisi. Vivamus ut neque nulla. est aliquam consectetur augue et est dapibus ;</p>
                 <p><a href="#" class="read-more">read more</a></p>
-            </div>
+            </div>-->
             
         </div>
     </div>  
